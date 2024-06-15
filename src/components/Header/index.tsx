f09194b1840252daaf5dfba07038bafc2e69c84c
dom-inic <dominicnyambane22@gmail.com>
2024-06-15 18:20:47 +0300

    (demo): logo bg

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -57,14 +57,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logomo.png"
+                  src="/images/logo/logomo-bgr.png"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logomo.png"
+                  src="/images/logo/logomo-bgr.png"
                   alt="logo"
                   width={140}
                   height={30}
